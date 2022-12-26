ae07a969aff8cca559ba9d28994963b15ef179b6
Immaculate Jelimo <immajelimo@Immaculates-MacBook-Pro.local>
2022-12-26 11:18:04 +0000

    added multiply_numbers funtion

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ print(add_numbers(a=5,b=10))
 def divide_numbers(a: int, b: int):
     return a / b
 print(divide_numbers(a=50, b=5))
+
+
+def multipy_numbers(a, b):
+    return a * b
+print(multipy_numbers(a= 23, b=22))
